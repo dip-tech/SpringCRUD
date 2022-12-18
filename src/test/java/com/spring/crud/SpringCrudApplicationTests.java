df79ca5cc50d1872c8ded54849cfde206ce1ce4d
Dipankar Dutta <dipankar8dutta@gmail.com>
2022-12-18 12:41:24 +0530

    COMMIT-18-12-2022:12-41

--- src/test/java/com/spring/crud/SpringCrudApplicationTests.java
+++ src/test/java/com/spring/crud/SpringCrudApplicationTests.java
@@ -1,14 +1,21 @@
 package com.spring.crud;
 
+import static io.restassured.RestAssured.given;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.restassured.RestAssured;
 
 @SpringBootTest
 class SpringCrudApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("Test Running");
+		RestAssured.baseURI = "http://localhost:8001/";
+		given().body("{\n" + "				    \"username\":\"dipankar8dutta@gmail.com\",\n"
+				+ "				    \"password\":\"dip@dutta\"\n" + "				}").when().post("user/login").then()
+				.assertThat().statusCode(200);
 	}
 
 }
