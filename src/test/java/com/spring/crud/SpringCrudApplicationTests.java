7b76c85ce1e1af983f649f5af3553d5a3124db3a
Dipankar Dutta <dipankar8dutta@gmail.com>
2022-12-18 14:49:27 +0530

    OK

--- src/test/java/com/spring/crud/SpringCrudApplicationTests.java
+++ src/test/java/com/spring/crud/SpringCrudApplicationTests.java
@@ -8,14 +8,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import io.restassured.RestAssured;
 
 @SpringBootTest
+
 class SpringCrudApplicationTests {
 
 	@Test
 	void contextLoads() {
-		RestAssured.baseURI = "http://localhost:8001/";
-		given().body("{\n" + "				    \"username\":\"dipankar8dutta@gmail.com\",\n"
-				+ "				    \"password\":\"dip@dutta\"\n" + "				}").when().post("user/login").then()
-				.assertThat().statusCode(200);
+		RestAssured.baseURI = "http://localhost:8001";
+		given().log().all().header("Content-Type", "application/json")
+				.body("{\n" + "				    \"username\":\"dipankar8dutta@gmail.com\",\n"
+						+ "				    \"password\":\"dip@dutta\"\n" + "				}")
+				.when().log().all().post("/user/login").then().assertThat().statusCode(200);
 	}
 
 }
